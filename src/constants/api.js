843d1c4a3cd2c33f30bd083dd259033ec6ded698
Viet1117 <vietpdph51809@gmail.com>
2025-08-17 23:20:27 +0700

    merge nam

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,22 +1,16 @@
 // API Configuration
 export const API_CONFIG = {
   // Thay đổi IP này theo IP máy tính của bạn
-<<<<<<< HEAD
-  BASE_URL: 'http://192.168.110.35:3000',
-=======
   BASE_URL: 'http://10.0.2.2:3000',
->>>>>>> Origin/Nam
   // Cho simulator/emulator
   LOCAL_URL: 'http://localhost:3000',
   // URL cho API bên ngoài
-  BASE_URL_EXTERNAL: 'https://389a5362809e.ngrok-free.app',
+  BASE_URL_EXTERNAL: 'https://290538b87be0.ngrok-free.app',
 
   // Endpoints
   ENDPOINTS: {
-    USERS: '/users',
-    ORDERS: '/orders',
-    STATISTICS: '/statistics',
-    NOTIFICATIONS: '/notifications',
+    USERS: '/api/shipper/getAllShipper',
+    ORDERS: '/api/order/getAll',
     
     // Order service endpoints
     ORDER_FILTER_BY_AREA: '/api/order/filterOrderAddressByCityAndWard',
